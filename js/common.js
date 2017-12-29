a5a91c15873ad97325c1aceced5d781c00c6a2a0
underbleu <underbleu.dev@gmail.com>
2017-12-29 18:38:39 +0900

    오목-승패결정 완성!!!!(아마도)

--- js/common.js
+++ js/common.js
@@ -1,12 +1,10 @@
 let user = 'black';
 let blackLocation = [];
 let whiteLocation = [];
-let cntB = 0;
-let cntW = 0;
+let blackID = [];
+let whiteID = [];
 
 // 오목판 그려주기
-
-
 
 // 오목돌 놓기
 document.addEventListener('DOMContentLoaded', () => {
@@ -30,35 +28,76 @@ document.addEventListener('DOMContentLoaded', () => {
             // 흰 검정돌 번갈아 놓기
             if(user === 'black'){
                 el.classList.add('black');
+                
+                // 좌표생성
                 blackLocation.push(locationXY);
-                console.log(blackLocation);
+                
+                //좌표를 활용하여 위치별 id값 생성
+                blackID.push((15 * (locationXY.y - 1) + 1 * locationXY.x));
+                blackID.sort((a, b) => a - b); // 화살표함수: 함수몸체 한 줄 표현식이면 "{return }"생략. return만 생략하고 {중괄호} 냅두면 sort함수 동작안함
+                
+                console.log(`blackID : ${blackID}`);
                 user = 'white'
             }else{
                 el.classList.add('white');
+
+                // 좌표생성
                 whiteLocation.push(locationXY);
-                console.log(whiteLocation);
+
+                // 좌표를 활용하여 위치별 id값 생성
+                whiteID.push((15 * (locationXY.y - 1) + 1 * locationXY.x));
+                whiteID.sort((a, b) => a - b); // 화살표함수: 함수몸체 한 줄 표현식이면 "{return }"생략. return만 생략하고 {중괄호} 냅두면 sort함수 동작안함
+
+                console.log(`whiteID : ${whiteID}`);
                 user = 'black';
             }
-
-
-            //좌표를 활용하여 돌 위치별 id값 생성
-            let blackID = [];
-            let whiteID = [];
-            
-            for (let item of blackLocation) {
-                blackID.push((15 * (item.y - 1) + 1 * item.x));
-            }
-            blackID.sort((a, b) => {a - b}); // 화살표함수: 함수몸체 한 줄 표현식이면 "{}, return"생략 가능
-            console.log(blackID);
-
-            for (let item of whiteLocation) {
-                whiteID.push((15 * (item.y - 1) + 1 * item.x));
-            }
-            whiteID.sort((a, b) => {a - b}); 
-            console.log(whiteID);
-                     
+                        
             // 승패 결정
+            for(let i = 0; i < blackID.length; i++){
 
+                // 좌우
+                if (blackID.includes(blackID[i] + 1) &&
+                    blackID.includes(blackID[i] + 2) &&
+                    blackID.includes(blackID[i] + 3) &&
+                    blackID.includes(blackID[i] + 4))
+                    {alert('좌우 검정 승리!'); return;}
+
+                // 상하
+                if (blackID.includes(blackID[i] + 15) &&     // col
+                    blackID.includes(blackID[i] + 30) && // col * 2
+                    blackID.includes(blackID[i] + 45) && // col * 3
+                    blackID.includes(blackID[i] + 60))   // col * 4
+                    { alert('상하 검정 승리!'); return; }
+
+                // 대각선
+                if (blackID.includes(blackID[i] + 16) && // (col + 1) * 1
+                    blackID.includes(blackID[i] + 32) && // (col + 1) * 2
+                    blackID.includes(blackID[i] + 48) && // (col + 1) * 3
+                    blackID.includes(blackID[i] + 64))    // (col + 1) * 4
+                    { alert('대각선 검정 승리!'); return; }
+            }
+            for (let i = 0; i < whiteID.length; i++) {
+
+                // 좌우
+                if (blackID.includes(whiteID[i] + 1) &&
+                    blackID.includes(whiteID[i] + 2) &&
+                    blackID.includes(whiteID[i] + 3) &&
+                    blackID.includes(whiteID[i] + 4)) { alert('좌우 검정 승리!'); return; }
+
+                // 상하
+                if (blackID.includes(whiteID[i] + 15) &&     // col
+                    blackID.includes(whiteID[i] + 30) && // col * 2
+                    blackID.includes(whiteID[i] + 45) && // col * 3
+                    blackID.includes(whiteID[i] + 60))   // col * 4
+                { alert('상하 검정 승리!'); return; }
+
+                // 대각선
+                if (blackID.includes(whiteID[i] + 16) && // (col + 1) * 1
+                    blackID.includes(whiteID[i] + 32) && // (col + 1) * 2
+                    blackID.includes(whiteID[i] + 48) && // (col + 1) * 3
+                    blackID.includes(whiteID[i] + 64))    // (col + 1) * 4
+                { alert('대각선 검정 승리!'); return; }
+            } 
         })
     })
 })
